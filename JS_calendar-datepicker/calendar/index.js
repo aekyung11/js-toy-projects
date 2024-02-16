90a713e67a6fc8e615a216ae4370f6bff2ce4490
Aekyung Yong <isaiah6014915@gmail.com>
2024-02-16 20:52:44 +0900

    feat: selected day support in calendar

--- JS_calendar-datepicker/calendar/index.js
+++ JS_calendar-datepicker/calendar/index.js
@@ -20,19 +20,23 @@ function toIsoDate(d) {
 
 class Calendar {
   constructor($container) {
-    const today = new Date();
-    const todayIsoDate = toIsoDate(today);
+    this.$container = $container;
+    this.today = new Date();
+    this.todayIsoDate = toIsoDate(this.today);
+    this.calendarDate = new Date(this.today);
+    this.$container.classList.add("calendar-container");
+    this.selectedIsoDate = null;
 
-    $container.insertAdjacentHTML(
+    this.$container.insertAdjacentHTML(
       "beforeend",
       `
       <div class="calendar-nav">
-        <i class="bx bxs-left-arrow"></i>
+        <i class="bx bxs-left-arrow pagination"></i>
         <div class="current-month-year">
           <span class="current-month"></span>
           <span class="current-year"></span>
         </div>
-        <i class="bx bxs-right-arrow"></i>
+        <i class="bx bxs-right-arrow pagination"></i>
       </div>
       <div class="calendar-grid">
         <span class="day-of-week">SUN</span>
@@ -46,17 +50,72 @@ class Calendar {
       `
     );
 
-    $container.classList.add("calendar-container");
-    const initialDate = new Date(today);
-    const year = initialDate.getFullYear();
-    const month = monthNames[initialDate.getMonth()];
+    const previousButton = this.$container.querySelector(".bxs-left-arrow");
+    const nextButton = this.$container.querySelector(".bxs-right-arrow");
 
-    $container.querySelector(".current-year").textContent = year;
-    $container.querySelector(".current-month").textContent = month;
+    previousButton.onclick = this.onPreviousMonth;
+    nextButton.onclick = this.onNextMonth;
+    this.updateCalendar();
 
-    const $calendarGrid = $container.querySelector(".calendar-grid");
+    this.grid = this.$container.querySelector(".calendar-grid");
+    $container.onclick = this.onClick;
+    $container.onmouseover = this.onMouseOver;
+    $container.onmouseout = this.onMouseOut;
+  }
 
-    const firstDayOfMonth = new Date(new Date(initialDate).setDate(1));
+  onMouseOver = (e) => {
+    if (e.target.dataset.isoDate !== undefined) {
+      e.target.classList.add("hovered");
+    }
+  };
+
+  onMouseOut = (e) => {
+    if (e.target.dataset.isoDate !== undefined) {
+      e.target.classList.remove("hovered");
+    }
+  };
+
+  onClick = (e) => {
+    if (e.target.dataset.isoDate !== undefined) {
+      if (this.selectedIsoDate) {
+        const $selectedDay = this.$container.querySelector(
+          `[data-iso-date='${this.selectedIsoDate}']`
+        );
+        if ($selectedDay) {
+          $selectedDay.classList.remove("selected");
+        }
+      }
+      this.selectedIsoDate = e.target.dataset.isoDate;
+      e.target.classList.add("selected");
+      console.log(e.target.dataset.isoDate);
+    }
+  };
+
+  onPreviousMonth = () => {
+    this.calendarDate.setMonth(this.calendarDate.getMonth() - 1);
+    this.updateCalendar();
+  };
+
+  onNextMonth = () => {
+    this.calendarDate.setMonth(this.calendarDate.getMonth() + 1);
+    this.updateCalendar();
+  };
+
+  updateCalendar() {
+    const days = this.$container.getElementsByClassName("day");
+    while (days.length > 0) {
+      days[0].parentNode.removeChild(days[0]);
+    }
+
+    const year = this.calendarDate.getFullYear();
+    const month = monthNames[this.calendarDate.getMonth()];
+
+    this.$container.querySelector(".current-year").textContent = year;
+    this.$container.querySelector(".current-month").textContent = month;
+
+    const $calendarGrid = this.$container.querySelector(".calendar-grid");
+
+    const firstDayOfMonth = new Date(new Date(this.calendarDate).setDate(1));
     const startDay = new Date(
       new Date(firstDayOfMonth).setDate(1 - firstDayOfMonth.getDay())
     );
@@ -86,8 +145,11 @@ class Calendar {
       if (currentDate.getMonth() !== firstDayOfMonth.getMonth()) {
         day.classList.add("prev-next-month-day");
       }
-      if (currentDateIsoDate === todayIsoDate) {
+      if (currentDateIsoDate === this.todayIsoDate) {
         day.classList.add("today");
+      }
+      if (this.selectedIsoDate === currentDateIsoDate) {
+        day.classList.add("selected");
       }
       day.textContent = currentDate.getDate();
       $calendarGrid.appendChild(day);
