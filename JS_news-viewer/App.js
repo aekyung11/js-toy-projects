69cfa3e9e52ddc7101aec06666db3f0f9ef244fd
Aekyung Yong <isaiah6014915@gmail.com>
2024-02-17 15:00:41 +0900

    feat: initial news components

--- JS_news-viewer/App.js
+++ JS_news-viewer/App.js
@@ -1,1 +1,11 @@
-// do something!
+import Nav from "./components/Nav.js";
+import NewsList from "./components/NewsList.js";
+
+const $root = document.querySelector("#root");
+const $nav = document.createElement("nav");
+const $newsList = document.createElement("div");
+
+$root.appendChild($nav);
+new Nav($nav);
+$root.appendChild($newsList);
+new NewsList($newsList);
